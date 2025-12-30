5b93b7558551edcf07546bb7f369cea440738f65
FungiMiracles <fungi.miracles@gmail.com>
2025-12-30 18:02:25 +0100

    Update demografia_utils.py

--- demografia_utils.py
+++ demografia_utils.py
@@ -3,8 +3,13 @@ def przelicz_region_demografia(region):
     Utrzymuje spójność:
     region_populacja = suma miast + ludność pozamiejska
     """
+
+    # 🔒 NORMALIZACJA NULL
+    region.region_populacja = region.region_populacja or 0
+    region.region_ludnosc_pozamiejska = region.region_ludnosc_pozamiejska or 0
+
     ludnosc_miast = sum(
-        m.miasto_populacja or 0
+        (m.miasto_populacja or 0)
         for m in region.miasta
     )
 
