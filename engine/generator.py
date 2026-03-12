1e04c4e0a9ac9ea5ac940b0358d906c538d47c38
Fungi Miracles <fungi.miracles@gmail.com>
2026-03-12 14:21:01 +0100

    Update generator.py

--- engine/generator.py
+++ engine/generator.py
@@ -40,7 +40,7 @@ def start_event_scheduler(app):
             print("[ZEUS] scheduler tick")
             generate_events()
 
-    scheduler.add_job(job, "interval", minutes=1)
+    scheduler.add_job(job, "interval", minutes=30)
 
     scheduler.start()
 
